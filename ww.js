20340c0ea2be8d0bad38f427815ffff96ab78514
蒋公石 <j6j7@qq.com>
2021-01-15 09:04:11 +0800

    autonavi

--- ww.js
+++ ww.js
@@ -1,4 +1,4 @@
-document.writeln("<link type=\"text/css\" rel=\"stylesheet\" href=\"animate.css\"/*tpa=https://js.eglobe.cn/wwall/style4/css/animate.css*/ />");
+document.writeln("<link type=\"text/css\" rel=\"stylesheet\" href=\"https://js.eglobe.cn/wwall/style4/css/animate.css\" />");
 document.writeln("<script src=\"https://js.eglobe.cn/wwall/style4/js/wow.min.js\"></script>");
 document.writeln("<script>");
 document.writeln("if (!(/msie [6|7|8|9]/i.test(navigator.userAgent))){new WOW().init();}");
